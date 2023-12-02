7a110498e4ba6a5ee875314ad4d2c331266d6c4c
LoofGodd <narot752@gmail.com>
2023-12-02 22:15:31 +0700

    Build search Input

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -9,7 +9,7 @@ export default function ColorModeSwitch() {
         colorScheme="green"
         onChange={toggleColorMode}
       />
-      <Text>Dark Mode</Text>
+      <Text whiteSpace="nowrap">Dark Mode</Text>
     </HStack>
   )
 }
